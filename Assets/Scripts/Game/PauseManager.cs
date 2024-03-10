2bf0b613b9da51a150c51361de2b32390385ab54
Nikita <zalesov.anatoly@yandex.ru>
2024-03-11 02:19:05 +0400

    core(Assets):Rework game, update game UI

---
+++ Assets/Scripts/Game/PauseManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    public bool GameIsPause
+    {
+        get
+        {
+            return this.isPaused;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void Restart()
+    {
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+}
